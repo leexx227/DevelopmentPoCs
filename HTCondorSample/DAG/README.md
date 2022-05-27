03c9d8544101be4652f56ca2378d1c753c4806ea
Xiang Li <xili10@microsoft.com>
2022-05-27 15:58:07 +0800

    Add a sample about DAG usage

---
+++ HTCondorSample/DAG/README.md
@@ -0,0 +1,6 @@
+This is a simple of DAG. It's in a diamond shape. Each node in the DAG runs the same job defined in `subjob.sub`.
+
+Usage:
+```
+condor_submit_dag diamond.dag
+```
---
+++ HTCondorSample/DAG/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import time
+import sys
+
+process = sys.argv[1]
+
+print("[{0}] DAG node {1} sleep...".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), process))
+time.sleep(10)
+print("[{0}] DAG node {1} awake...".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),process))
